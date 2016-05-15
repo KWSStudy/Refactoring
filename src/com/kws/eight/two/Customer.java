de4261c9d0ed34deac554d1f37c6f8c4be9fec15
Administrator <Administrator@MSDN-SPECIAL>
2016-05-15 23:42:35 +0900

    8장 데이터체계화

---
+++ src/com/kws/eight/two/Customer.java
@@ -0,0 +1,30 @@
+package com.kws.eight.two;
+
+public class Customer {
+	private final String _customer;
+	private final int _phone;
+	private final String _address;
+
+	public Customer(String customer, int phone, String address) {
+		this._customer = customer;
+		this._phone = phone;
+		this._address = address;
+	}
+
+	public Customer getCustomers() {
+		return this;
+	}
+	
+	public String getCustomer(){
+		return this._customer;
+	}
+	
+	public int getPhone(){
+		return this._phone;
+	}
+	
+	public String getAddress(){
+		return this._address;
+	}
+
+}
